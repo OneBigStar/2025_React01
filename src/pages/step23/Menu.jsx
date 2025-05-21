efa77deb734acaf5148a2133d5510777c1295635
deokkyu <han14suk@gmail.com>
2025-05-21 15:30:56 +0900

    0521_지각함함

--- src/pages/step23/Menu.jsx
+++ src/pages/step23/Menu.jsx
@@ -1,21 +1,35 @@
+import { Link } from "react-router-dom";
+import './Menu.css'
 
 export default function Menu(){
-    return (
-        <nav className="nav">
+    return(
+       <nav className="nav">
             <ul>
                 <li className="lineStyle">
-                    <Link to="/first">홈홈</Link>
+                    <Link to="/">홈</Link>
                 </li>
                 <li className="lineStyle">
-                    <Link to="/second">첫번째</Link>
-                </li>
+                    <Link to="/first">첫번째</Link>
+                </li> 
                 <li className="lineStyle">
-                    <Link to="/thired">두번째</Link>
-                </li>
+                    <Link to="/second">두번째</Link>
+                </li>  
                 <li className="lineStyle">
-                    <Link to="/fourth">세번째</Link>
-                </li>
+                    <Link to="/third">세번째</Link>
+                </li> 
+                <li className="lineStyle">
+                    <Link to="/fetchEx01">온도-1</Link>
+                </li> 
+                <li className="lineStyle">
+                    <Link to="/fetchEx02">온도-2</Link>
+                </li> 
+                <li className="lineStyle">
+                    <Link to="/fetchEx03">온도-3</Link>
+                </li> 
+                <li className="lineStyle">
+                    <Link to="/movielist">movieList</Link>
+                </li> 
             </ul>
-        </nav>
+       </nav> 
     )
 }
